94c66b57f91d3c17803155f106e1ef277d4d8881
Derek <dt.derek.tam@gmail.com>
2021-01-18 13:06:05 -0500

    scripts

---
+++ bin/hs_normal_train.sh
@@ -0,0 +1,6 @@
+bash bin/normal_train.sh processed_data/hs/ 32 None 10000
+bash bin/normal_train.sh processed_data/hs/ 32 SynonymReplacement 10000
+bash bin/normal_train.sh processed_data/hs/ 32 WordReplacementVocab 10000
+bash bin/normal_train.sh processed_data/hs/ 32 RandomInsertion 10000
+bash bin/normal_train.sh processed_data/hs/ 32 RandomDeletion 10000
+bash bin/normal_train.sh processed_data/hs/ 32 RandomSwapping 10000
--- bin/setup.sh
+++ bin/setup.sh
@@ -1,3 +1,4 @@
-export DT_ROOT=`pwd`
+source env/bin/activate
+export DA_ROOT=`pwd`
 export PYTHONPATH=$DA_ROOT:$PYTHONPATH
 export PYTHON_EXEC=python
