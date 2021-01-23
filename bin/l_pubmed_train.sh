ff89cb8ff724646e67401f1a9150e8ed01f13b9d
Derek <dt.derek.tam@gmail.com>
2021-01-23 09:05:06 -0500

    pubmed ssl

---
+++ bin/l_pubmed_train.sh
@@ -0,0 +1,7 @@
+bash bin/train.sh processed_data/pubmed/ 3 6 SynonymReplacement 10 1
+bash bin/train.sh processed_data/pubmed/ 3 6 WordReplacementLM 10 1
+bash bin/train.sh processed_data/pubmed/ 3 6 RandomInsertion 10 1
+bash bin/train.sh processed_data/pubmed/ 3 6 RandomDeletion 10 1
+bash bin/train.sh processed_data/pubmed/ 3 6 RandomSwapping 10 1
+bash bin/train.sh processed_data/pubmed/ 3 6 WordReplacementVocab 10 1
+bash bin/train.sh processed_data/pubmed/ 3 6 Cutoff 10 1
