1a9535b8e2037b374efcaa6df797161a9077ae52
Derek <dt.derek.tam@gmail.com>
2021-01-27 15:55:18 -0500

    ci

--- code/get_scores.py
+++ code/get_scores.py
@@ -31,8 +31,8 @@ def get_dict_test_scores(exp_dir):
     print(dict_seed_to_test_acc)
 
     average = np.mean(np.asarray(list(dict_seed_to_test_f1.values())))
-    std_dev = scipy.stats.sem(np.asarray(list(dict_seed_to_test_f1.values())))
-    ci = std_error * scipy.stats.t.ppf((1 + 0.95) / 2., n - 1)
+    std_dev = stats.sem(np.asarray(list(dict_seed_to_test_f1.values())))
+    ci = std_error * stats.t.ppf((1 + 0.95) / 2., n - 1)
 
     print("Average: %.3f, Std Dev: %.3f, CI: %.3f " % (average, std_dev, ci))
     print(dict_seed_to_test_f1)
