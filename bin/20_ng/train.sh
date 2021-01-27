c1234d12f64e7060cac01d5c89f703f0f1abe0ce
Derek <dt.derek.tam@gmail.com>
2021-01-27 00:33:04 -0500

    train

--- bin/20_ng/train.sh
+++ bin/20_ng/train.sh
@@ -3,19 +3,17 @@
 for i in 42
 do
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
 
 done
 
@@ -23,19 +21,35 @@ done
 for i in 42
 do
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+done
+
+
+for i in 0 1
+do
+
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+
+    python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
 
 done
 
@@ -45,40 +59,16 @@ done
 for i in 0 1
 do
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
 
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
-
-done
-
-
-
-
-for i in 0 1
-do
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=$i
-
-    CUDA_VISIBLE_DEVICES=7 python -m code.train -c ./config/ag_news/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+    python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
 
 done
