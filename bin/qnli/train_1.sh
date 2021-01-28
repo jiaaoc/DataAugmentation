9af37750c9bcdf9cfc2b4aaf4fc462ccad3bac70
Derek <dt.derek.tam@gmail.com>
2021-01-28 00:03:03 -0500

    qnli rte

---
+++ bin/qnli/train_1.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+for i in 1
+do
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=$i
+
+    python -m code.train -c ./config/qnli/10_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=$i
+
+    python -m code.train -c ./config/qnli/100_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=$i
+
+done
