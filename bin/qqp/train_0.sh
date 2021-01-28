1b0060ecc5f178a7152f83c52b3428b77ba34047
Derek <dt.derek.tam@gmail.com>
2021-01-28 00:10:15 -0500

    qqp

---
+++ bin/qqp/train_0.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+for i in 0
+do
+
+    python -m code.train -c ./config/qqp/10_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+
+    python -m code.train -c ./config/qqp/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+
+    python -m code.train -c ./config/qqp/10_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+
+    python -m code.train -c ./config/qqp/10_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+
+    python -m code.train -c ./config/qqp/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+
+    python -m code.train -c ./config/qqp/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+
+    python -m code.train -c ./config/qqp/100_lbl_5000_unlbl.json -k transform_type=SynonymReplacement seed=$i
+
+    python -m code.train -c ./config/qqp/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=$i
+
+    python -m code.train -c ./config/qqp/100_lbl_5000_unlbl.json -k transform_type=RandomInsertion seed=$i
+
+    python -m code.train -c ./config/qqp/100_lbl_5000_unlbl.json -k transform_type=RandomDeletion seed=$i
+
+    python -m code.train -c ./config/qqp/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=$i
+
+    python -m code.train -c ./config/qqp/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=$i
+
+done
