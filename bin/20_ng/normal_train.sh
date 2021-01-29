690bec57e1dceb13e25faa8285b29b938a12077b
Derek <dt.derek.tam@gmail.com>
2021-01-29 11:03:42 -0500

    20_ng

--- bin/20_ng/normal_train.sh
+++ bin/20_ng/normal_train.sh
@@ -2,54 +2,13 @@
 
 for i in 0 1 42
 do
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=None seed=$i
 
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=SynonymReplacement seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=RandomInsertion seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=RandomDeletion seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=RandomSwapping seed=$i
 
     python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=Cutoff seed=$i
 
     python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=$i
 
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=$i
 
 done
-
-
-for i in 0 1 42
-do
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=None seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=SynonymReplacement seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=RandomInsertion seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=RandomDeletion seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=RandomSwapping seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=Cutoff seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=$i
-
-    python -m code.normal_train -c ./config/20_ng/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=$i
-
-
-done
-
-
-
-
-
-
 
 
