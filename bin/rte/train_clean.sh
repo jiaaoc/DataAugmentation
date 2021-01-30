5b38997bfc9592eacafe52dc83059c7a9ab8d852
Derek <dt.derek.tam@gmail.com>
2021-01-30 15:27:10 -0500

    fix rte

--- bin/rte/train_clean.sh
+++ bin/rte/train_clean.sh
@@ -2,13 +2,13 @@
 
 
 
-python -m code.train -c ./config/rte/10_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=0
+python -m code.train -c ./config/rte/100_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=0
 
-python -m code.train -c ./config/rte/10_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=42
+python -m code.train -c ./config/rte/100_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=42
 
-python -m code.train -c ./config/rte/10_lbl_5000_unlbl.json -k transform_type=Cutoff seed=0
+python -m code.train -c ./config/rte/100_lbl_5000_unlbl.json -k transform_type=Cutoff seed=0
 
-python -m code.train -c ./config/rte/10_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=0
+python -m code.train -c ./config/rte/100_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=0
 
-python -m code.train -c ./config/rte/10_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=42
+python -m code.train -c ./config/rte/100_lbl_5000_unlbl.json -k transform_type=BackTranslation seed=42
 
