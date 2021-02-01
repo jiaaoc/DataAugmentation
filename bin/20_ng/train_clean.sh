90660875d5fd411c656008d550943bb8a66f140f
Derek <dt.derek.tam@gmail.com>
2021-01-31 22:14:45 -0500

    20 ng

--- bin/20_ng/train_clean.sh
+++ bin/20_ng/train_clean.sh
@@ -2,9 +2,9 @@
 
 
 
-python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=0
+python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=0
 
-python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=1
+python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=1
 
-python -m code.train -c ./config/20_ng/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=42
+python -m code.train -c ./config/20_ng/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=42
 
