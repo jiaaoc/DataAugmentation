885ee04c234c9af5b9e3d67f1e792407e34595b6
Derek <dt.derek.tam@gmail.com>
2021-02-01 17:13:56 -0500

    mnli

--- bin/mnli/clean_normal_train.sh
+++ bin/mnli/clean_normal_train.sh
@@ -1,18 +1,5 @@
 #!/usr/bin/env bash
 
-
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=0
-
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=1
-
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=42
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=0
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=1
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=42
-
 python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=42
 
 
--- bin/mnli/clean_train.sh
+++ bin/mnli/clean_train.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
 
-python -m code.train -c ./config/mnli/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=0
+python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=0
 
-python -m code.train -c ./config/mnli/10_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=1
+python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=1
 
-
-python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=0
-
-python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=1
+python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=RandomSwapping seed=42
 
 python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementLM seed=42
-
-python -m code.train -c ./config/mnli/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=0
-
-python -m code.train -c ./config/mnli/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=1
-
-python -m code.train -c ./config/mnli/10_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=42
-
-python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=0
-
-python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=1
-
-python -m code.train -c ./config/mnli/100_lbl_5000_unlbl.json -k transform_type=WordReplacementVocab seed=42
-
