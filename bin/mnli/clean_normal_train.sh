8ca6b27a451177cad5160f0f403e4728473c2141
Derek <dt.derek.tam@gmail.com>
2021-02-01 11:06:35 -0500

    mnli

--- bin/mnli/clean_normal_train.sh
+++ bin/mnli/clean_normal_train.sh
@@ -1,32 +1,17 @@
 #!/usr/bin/env bash
 
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=0
 
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=1
+python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=0
 
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=42
+python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=1
 
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=0
+python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=BackTranslation seed=42
 
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=1
+python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=0
 
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementVocab seed=42
+python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=1
 
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=RandomSwapping seed=0
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=RandomSwapping seed=1
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=RandomSwapping seed=42
-
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=0
-
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=1
-
-python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=42
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=0
-
-python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=1
+python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=BackTranslation seed=42
 
 python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=WordReplacementLM seed=42
 
