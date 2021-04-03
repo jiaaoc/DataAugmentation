20d33ed22bd241bf013c534165379c315110e9f3
Derek <dt.derek.tam@gmail.com>
2021-04-02 23:27:07 -0400

    20ng

---
+++ bin/20_ng/adv_mixup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for i in 0 1 42
+do
+
+    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
+
+    python -m code.normal_train -c ./config/20_ng/10_lbl_0_unlbl.json -k transform_type=None emb_aug=mixup seed=$i
+
+done
+
+
