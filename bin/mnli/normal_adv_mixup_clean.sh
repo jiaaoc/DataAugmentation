8def0b4d40e9de30cd1c4bb4b959298e331a6191
Derek <dt.derek.tam@gmail.com>
2021-04-06 08:59:32 -0400

    clean

--- bin/mnli/normal_adv_mixup_clean.sh
+++ bin/mnli/normal_adv_mixup_clean.sh
@@ -2,13 +2,9 @@
 
 for i in 0 1 42
 do
-    python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
 
-    python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=None emb_aug=mixup seed=$i
+    python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
 
-    python -m code.normal_train -c ./config/mnli/10_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
-
-    python -m code.normal_train -c ./config/mnli/100_lbl_0_unlbl.json -k transform_type=None emb_aug=mixup seed=$i
 
 done
 
--- bin/qqp/normal_adv_mixup_clean.sh
+++ bin/qqp/normal_adv_mixup_clean.sh
@@ -2,13 +2,8 @@
 
 for i in 0 1 42
 do
-    python -m code.normal_train -c ./config/qqp/10_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
 
-    python -m code.normal_train -c ./config/qqp/10_lbl_0_unlbl.json -k transform_type=None emb_aug=mixup seed=$i
-
-    python -m code.normal_train -c ./config/qqp/10_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
-
-    python -m code.normal_train -c ./config/qqp/100_lbl_0_unlbl.json -k transform_type=None emb_aug=mixup seed=$i
+    python -m code.normal_train -c ./config/qqp/100_lbl_0_unlbl.json -k transform_type=None emb_aug=adv seed=$i
 
 done
 
