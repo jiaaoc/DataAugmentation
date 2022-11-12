785c869b5e676b8e31924fb30b76447ce3fdec37
Derek Tam <dt.derek.tam@gmail.com>
2022-11-11 16:54:29 -0800

    add new scripts

--- data/generate_lbl_pickle.py
+++ data/generate_lbl_pickle.py
@@ -6,15 +6,14 @@ import csv
 import codecs
 
 from code.read_data import get_data
-from sklearn.datasets import fetch_20newsgroups
 from code.Config import Config
 
 
-def pickle_mrpc(num_lbl):
-    config = Config(f"config/mrpc/{num_lbl}_lbl_0_unlbl.json", {})
+def pickle_dataset(config_dataset, processed_dataset, num_lbl):
+    config = Config(f"config/{config_dataset}/{num_lbl}_lbl_0_unlbl.json", {})
     train_labeled_set, _, _, _, _ = get_data(config)
 
-    train_pk_file = os.path.join("processed_data", "MRPC",  f"train_{num_lbl}_labeled_data.pkl")
+    train_pk_file = os.path.join("processed_data", f"{processed_dataset}",  f"train_{num_lbl}_labeled_data.pkl")
     dict_train_input = {}
     ctr = 0
 
@@ -25,28 +24,32 @@ def pickle_mrpc(num_lbl):
         pickle.dump(dict_train_input, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 
-def generate_pickle(dataset):
-    # if dataset == "20_ng":
-    #     pickle_20_ng()
-    # elif dataset == "pubmed":
-    #     pickle_pubmed()
-    # elif dataset == "mnli":
-    #     pickle_mnli()
-    # elif dataset == "rte":
-    #     pickle_rte()
-    # elif dataset == "qnli":
-    #     pickle_qnli()
-    # elif dataset == "qqp":
-    #     pickle_qqp()
-    if dataset == "mrpc":
-        pickle_mrpc(10)
+def generate_pickle():
+
+    pickle_dataset("20_ng", "20_ng", 10)
+    pickle_dataset("20_ng", "20_ng", 100)
+
+    pickle_dataset("pubmed", "pubmed", 10)
+    pickle_dataset("pubmed", "pubmed", 100)
+
+    pickle_dataset("mnli", "MNLI", 10)
+    pickle_dataset("mnli", "MNLI", 100)
+
+    pickle_dataset("qnli", "QNLI", 10)
+    pickle_dataset("qnli", "QNLI", 100)
+
+    pickle_dataset("qqp", "QQP", 10)
+    pickle_dataset("qqp", "QQP", 100)
+
+    pickle_dataset("mrpc", "MRPC", 10)
+    pickle_dataset("mrpc", "MRPC", 100)
 
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', "--dataset", required=True)
+    parser.add_argument('-d', "--dataset")
     args = parser.parse_args()
 
-    generate_pickle(args.dataset)
+    generate_pickle()
--- data/prompt_pred.py
+++ data/prompt_pred.py
@@ -55,7 +55,132 @@ def format_mrpc(train_labeled_data, num_examples):
         raise ValueError("Invalid Output")
     return random_input, new_output, total_input
 
-def prompt_pred(data_path, num_lbl, device):
+def format_qqp(train_labeled_data, num_examples):
+    total_input = ""
+    for datapoint in train_labeled_data[:num_examples]:
+        input = datapoint[0]
+        output = datapoint[1]
+        if output == 0:
+            prompt_template = f"{input[0]} is not semantically equivalent to {input[1]}."
+        elif output == 1:
+            prompt_template = f"{input[0]} is semantically equivalent to {input[1]}."
+        else:
+            raise ValueError("Invalid Output")
+
+        total_input += " " + prompt_template
+    new_example = train_labeled_data[num_examples]
+    new_input = new_example[0]
+    random_input = random.choice(new_input)
+    new_output = new_example[1]
+    if new_output == 0:
+        total_input += f"{random_input} is not semantically equivalent to "
+    elif new_output == 1:
+        total_input += f"{random_input} is semantically equivalent to "
+    else:
+        raise ValueError("Invalid Output")
+    return random_input, new_output, total_input
+
+def format_qnli(train_labeled_data, num_examples):
+    total_input = ""
+    for datapoint in train_labeled_data[:num_examples]:
+        input = datapoint[0]
+        output = datapoint[1]
+        if output == 0:
+            prompt_template = f"{input[0]} does not imply {input[1]}."
+        elif output == 1:
+            prompt_template = f"{input[0]} implies {input[1]}."
+        else:
+            raise ValueError("Invalid Output")
+
+        total_input += " " + prompt_template
+    new_example = train_labeled_data[num_examples]
+    new_input = new_example[0]
+    new_output = new_example[1]
+    if new_output == 0:
+        total_input += f"{new_input[0]} does not imply"
+    elif new_output == 1:
+        total_input += f"{new_input[0]} implies "
+    else:
+        raise ValueError("Invalid Output")
+    return new_input[0], new_output, total_input
+
+def format_20ng(train_labeled_data, num_examples):
+    total_input = ""
+    for datapoint in train_labeled_data[:num_examples]:
+        input = datapoint[0]
+        output = datapoint[1]
+        if output == 0:
+            prompt_template = f""
+        elif output == 1:
+            prompt_template = f""
+        else:
+            raise ValueError("Invalid Output")
+
+        total_input += " " + prompt_template
+    new_example = train_labeled_data[num_examples]
+    new_input = new_example[0]
+    new_output = new_example[1]
+    if new_output == 0:
+        total_input += f""
+    elif new_output == 1:
+        total_input += f""
+    else:
+        raise ValueError("Invalid Output")
+    return new_input[0], new_output, total_input
+
+def format_pubmed(train_labeled_data, num_examples):
+    total_input = ""
+    for datapoint in train_labeled_data[:num_examples]:
+        input = datapoint[0]
+        output = datapoint[1]
+        if output == 0:
+            prompt_template = f""
+        elif output == 1:
+            prompt_template = f""
+        else:
+            raise ValueError("Invalid Output")
+
+        total_input += " " + prompt_template
+    new_example = train_labeled_data[num_examples]
+    new_input = new_example[0]
+    new_output = new_example[1]
+    if new_output == 0:
+        total_input += f""
+    elif new_output == 1:
+        total_input += f""
+    else:
+        raise ValueError("Invalid Output")
+    return new_input[0], new_output, total_input
+
+def format_mnli(train_labeled_data, num_examples):
+    total_input = ""
+    for datapoint in train_labeled_data[:num_examples]:
+        input = datapoint[0]
+        output = datapoint[1]
+        if output == 0:
+            prompt_template = f"{input[0]} does not imply {input[1]}."
+        elif output == 1:
+            prompt_template = f"{input[0]} is neutral with {input[1]}."
+        elif output == 2:
+            prompt_template = f"{input[0]} implies {input[1]}."
+        else:
+            raise ValueError("Invalid Output")
+
+        total_input += " " + prompt_template
+    new_example = train_labeled_data[num_examples]
+    new_input = new_example[0]
+    new_output = new_example[1]
+    if new_output == 0:
+        total_input += f"{new_input[0]} does not imply"
+    elif new_output == 1:
+        total_input += f"{new_input[0]} is neutral with "
+    elif new_output == 2:
+        total_input += f"{new_input[0]} implies "
+    else:
+        raise ValueError("Invalid Output")
+    return new_input[0], new_output, total_input
+
+def prompt_pred(data_path, dataset, num_lbl, device):
     transformer = AutoModelForCausalLM.from_pretrained("gpt2").to(device)
     transformer.eval()
     tokenizer = AutoTokenizer.from_pretrained("gpt2")
@@ -67,7 +192,19 @@ def prompt_pred(data_path, num_lbl, device):
     all_input = []
     for i in range(1000):
         random.shuffle(train_labeled_data)
-        random_input, new_output, total_input = format_mrpc(train_labeled_data, 5)
+        if dataset == "mrpc":
+            random_input, new_output, total_input = format_mrpc(train_labeled_data, 5)
+        elif dataset == "qqp":
+            random_input, new_output, total_input = format_qqp(train_labeled_data, 5)
+        elif dataset == "qnli":
+            random_input, new_output, total_input = format_qnli(train_labeled_data, 5)
+        elif dataset == "mnli":
+            random_input, new_output, total_input = format_mnli(train_labeled_data, 5)
+        elif dataset == "pubmed":
+            random_input, new_output, total_input = format_pubmed(train_labeled_data, 5)
+        elif dataset == "20_ng":
+            random_input, new_output, total_input = format_20ng(train_labeled_data, 5)
+
         all_input.append(total_input)
         all_prompt_input.append((random_input, new_output))
     batch_size = 1
@@ -99,7 +236,8 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('-g', "--device", default=0, type=int)
     parser.add_argument('-n', "--num_label", default=10, type=int)
-
+    parser.add_argument("--dataset", dataset=["mrpc", "qqp", "qnli", "mnli", "pubmed",
+                                                      "20_ng"])
     parser.add_argument('-d', '--datapath', type=str, default='./processed_data/',
                         help='path to data folders')
     args = parser.parse_args()
@@ -107,7 +245,7 @@ if __name__ == "__main__":
 
     device = torch.device("cuda:%d" % args.device if torch.cuda.is_available() else "cpu")
     random.seed(0)
-    prompt_pred(args.datapath, args.num_label, device)
+    prompt_pred(args.datapath, args.dataset, args.num_label, device)
 
 
 
